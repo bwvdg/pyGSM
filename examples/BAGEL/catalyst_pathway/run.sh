6bc659cde9b24ab3012d5fb36791af1f2fe20fef
Beatrice Van Der Goetz <vandergoetz@qsimulate.com>
2022-05-17 18:17:01 +0000

    updated example with output

--- examples/BAGEL/catalyst_pathway/run.sh
+++ examples/BAGEL/catalyst_pathway/run.sh
@@ -7,4 +7,4 @@ BAGEL_NUM_THREADS=8
 gsm -xyzfile path_inp.xyz -restart_file path_inp.xyz -mode DE_GSM -package BAGEL \
   -lot_inp_file "$(pwd)/bagel.json" -bagel_runpath "$(pwd)/bagel.sh" \
   -reactant_geom_fixed -product_geom_fixed -mp_cores $OMP_NUM_THREADS -num_nodes 30 -max_opt_steps 3 \
-  -CONV_TOL 0.003 -max_gsm_iters 40 
+  -CONV_TOL 0.003 -max_gsm_iters 40 > gsm.out
